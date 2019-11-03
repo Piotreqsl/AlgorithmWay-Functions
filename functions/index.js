b71cafcc0823665c5d15aed97513b86200714fa1
Piotr <piotreksl2002@go2.pl>
2019-11-03 12:33:46 +0100

    Dodany express.js do functions (instalnij se do node modules) i teraz można brać posty z danej kolekcji tylko w linku z api, czyli bierzesz link z firebase i na końcu dajesz /Posts i działa

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,26 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+const express = require("express");
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello worlds!123s");
+app.get("/Posts", (req, res) => {
+  admin
+    .firestore()
+    .collection("Posts")
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      });
+      return res.json(posts);
+    })
+    .catch(err => console.error(err));
 });
 
-
-exports.getPosts = functions.https.onRequest((req, res) => {
-    admin.firestore().collection("Posts").get().then((data) => {
-            let posts = [];
-            data.forEach((doc) => {
-                posts.push(doc.data());
-            });
-            return res.json(posts);
-
-        })
-        .catch((err) => console.error(err));
-})
-
+exports.api = functions.https.onRequest(app);
 
 //PIOTREK KURWA ZOBACZ TO PANI P NA GOLAJA
